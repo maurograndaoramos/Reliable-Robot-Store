ce8576e71cc78ed23581cc0d06353f4c5e1e8a37
Mauro Ramos <mauro.dgramos@gmail.com>
2025-02-24 17:14:46 +0000

    [Fix] Just updated PR.yml one last time and added Buttons to pages

--- makefile
+++ makefile
@@ -1,3 +1,5 @@
+# Currently not in use as I am using git commands directly for testing purposes, but this should contain the commands that would help the developer automate the majority of required processes for CI/CD.
+
 dev:
 	git checkout -b dev
 
--- src/app/[tag]/products/page.tsx
+++ src/app/[tag]/products/page.tsx
@@ -1,5 +1,6 @@
 import ProductCard from '@/components/ProductsPage/ProductCard';
 import ErrorPage from '@/components/Error';
+import Link from 'next/link';
 import { Product, Params } from '@/lib/interfaces';
 import { fetchAllProducts } from '@/lib/shopify';
 
@@ -30,23 +31,32 @@ export default async function ProductsPage({
     console.error(error);
     return <ErrorPage error={ERROR_MESSAGE} />;
   }
-
   return (
-    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
-      {products.map((product) => {
-        const lowestPrice =
-          product.variants?.length
-            ? Math.min(...product.variants.map((variant) => parseFloat(variant.price)))
-            : 0;
-        return (
-          <ProductCard
-            key={product.id}
-            product={product}
-            lowestPrice={lowestPrice}
-            tag={tag}
-          />
-        );
-      })}
-    </div>
+    <>
+      <Link href="/">
+        <button className="bg-blue-600 text-white px-4 py-2 rounded hover:bg-blue-700 transition-colors">
+          Back
+        </button>
+      </Link>
+      <div className="mt-6 mb-6 p-4 bg-white shadow-lg border border-gray-200 rounded-lg text-xl font-semibold text-gray-800">
+        Total Available {tag} Products in Stock: {totalInventory}
+      </div>
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
+        {products.map((product) => {
+          const lowestPrice =
+            product.variants?.length
+              ? Math.min(...product.variants.map((variant) => parseFloat(variant.price)))
+              : 0;
+          return (
+            <ProductCard
+              key={product.id}
+              product={product}
+              lowestPrice={lowestPrice}
+              tag={tag}
+            />
+          );
+        })}
+      </div>
+    </>
   );
 }
