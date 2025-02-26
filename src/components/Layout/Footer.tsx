ee387790c82c1761e7fa6f9266fb7b41fd65af8e
maurograndaoramos <mauro.dgramos@gmail.com>
2025-02-26 19:42:52 +0000

    [feat] Changed footer information

--- src/components/Layout/Footer.tsx
+++ src/components/Layout/Footer.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Footer: React.FC = () => {
     return (
         <footer style={{ left: 0, bottom: 0, width: '100%', padding: '1rem', textAlign: 'center', backgroundColor: '#f5f5f5' }}>
-            <p>&copy; {new Date().getFullYear()} Just a mockfooter.</p>
+            <p>&copy; {new Date().getFullYear()} Just a fake footer.</p>
         </footer>
     );
 };
